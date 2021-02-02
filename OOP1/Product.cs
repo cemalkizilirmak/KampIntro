5fdc12cea593369946fb65d42e7f05a5eca47143
cemal kızılırmak <cemalkizilirmak@gmail.com>
2021-02-02 15:50:42 +0300

    20.01.2021 Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        //Id'si
+        public int Id { get; set; }
+        //Hangi katogoriye ait olduğunu
+        public int CategoryId { get; set; }
+        //Ürün ismi
+        public string ProductName { get; set; }
+        //Ürünün birim fiyatı
+        public double UnitPrice { get; set; }
+        //Ürünün stok adedi
+        public int UnitInStock { get; set; }
+
+        
+
+    }
+}
